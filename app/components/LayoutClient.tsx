caad3aa9316b0bf1444d262678f05f4c0ac3e9ec
전세계 <kssjssk@gmail.com>
2025-02-22 15:50:29 +0900

    fix/ 최상위 use client로 인한  hydration 해결

---
+++ app/components/LayoutClient.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { useGetHeaderTitle } from "@/utils/getHeaderTitle";
+import { usePathname } from "next/navigation";
+import { ContentContainer, RootContainer } from "@/app/components/RootContainerStyled";
+import Header from "@/app/components/Header";
+
+const LayoutClient = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const title = useGetHeaderTitle(pathname);
+  const isBackType = title !== "";
+
+  return (
+    <RootContainer>
+      <Header type={isBackType ? "back" : "default"} title={title} />
+      <ContentContainer>{children}</ContentContainer>
+    </RootContainer>
+  );
+};
+export default LayoutClient;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,17 @@
-"use client";
-
 import "@/styles/globals.css";
-import Header from "@/app/components/Header";
-import { usePathname } from "next/navigation";
 import StyledComponentsRegistry from "@/lib/StyledComponentsRegistry";
-import { useGetHeaderTitle } from "@/utils/getHeaderTitle";
-import {
-  ContentContainer,
-  RootContainer,
-} from "./components/RootContainerStyled";
+import LayoutClient from "./components/LayoutClient";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-
-  const title = useGetHeaderTitle(pathname);
-
-  const isBackType = title !== "";
-
   return (
     <html lang="en">
       <body>
         <StyledComponentsRegistry>
-          <RootContainer>
-            <Header type={isBackType ? "back" : "default"} title={title} />
-            <ContentContainer>{children}</ContentContainer>
-          </RootContainer>
+          <LayoutClient> {children} </LayoutClient>
         </StyledComponentsRegistry>
       </body>
     </html>
