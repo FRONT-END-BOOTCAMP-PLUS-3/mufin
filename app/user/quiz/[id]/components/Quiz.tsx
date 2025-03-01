242dd2ea795b4685ff158b3d3ee66bcae29771f1
전세계 <kssjssk@gmail.com>
2025-03-02 00:43:22 +0900

    refactor : #31 import 중복 제거를 위한 리펙토링

--- app/user/quiz/[id]/components/Quiz.tsx
+++ app/user/quiz/[id]/components/Quiz.tsx
@@ -1,13 +1,8 @@
 "use client";
-import {QuizButtonWrapper, QuizContainer, TextGroup} from "@/app/user/quiz/[id]/components/Quiz.Styled";
-import BaseButton from "@/app/user/quiz/[id]/components/BaseButton";
-
-import ProgressBar from "@/app/user/quiz/[id]/components/ProgressBar";
-import OXButtons from "@/app/user/quiz/[id]/components/OXButtons";
-import OptionButtons from "@/app/user/quiz/[id]/components/OptionButtons";
 import { useState } from "react";
-import { mockQuizData, QuizData } from "@/app/user/quiz/[id]/components/mockData";
-import BaseModal from "@/app/user/quiz/[id]/components/BaseModal";
+import { ProgressBar, OXButtons, OptionButtons, BaseModal, BaseButton } from "@/app/user/quiz/[id]/components";
+import { QuizButtonWrapper, QuizContainer, TextGroup } from "@/app/user/quiz/[id]/components/Quiz.Styled";
+import { mockQuizData, QuizData, } from "@/app/user/quiz/[id]/components/mockData";
 import { useRouter } from "next/navigation";
 
 // 문제 5개를 가져와서 한페이지 안에서 출력
@@ -28,29 +23,31 @@ const Quiz = () => {
 
   // 정답 확인 함수
   const handleResult = () => {
+    // 모달 open
     setIsModalOpen(true);
-    // 모달 오픈
+    
     // 정답 확인해서 모달에 전달
     const correct = selectedOption === currentQuestion.answer;
     setIsCorrect(correct);
+    
     if (correct) {
-        // 사용자가 푼 문제가 정답이면 server에 퀴즈 id를 전달
-        setScore((prev) => prev + 1);
-
+      // 사용자가 푼 문제가 정답이면 server에 퀴즈 id를 전달
+      setScore((prev) => prev + 1);
     }
   };
 
+  // 다음 문제 넘어가기
   const handleNext = () => {
     setIsModalOpen(false);
     if (currentIndex < totalQuestions - 1) {
       setCurrentIndex((prev) => prev + 1);
       setSelectedOption(0);
-    } else if (currentIndex === totalQuestions - 1){
+    } else if (currentIndex === totalQuestions - 1) {
       setIsLast(true);
     } else {
       handleOut();
     }
-  }
+  };
 
   const handleOut = () => {
     setIsModalOpen(false);
@@ -58,20 +55,26 @@ const Quiz = () => {
     router.push("/");
   };
 
+  // 조건부 렌더링 
   const renderChoices = () => {
     return currentQuestion.choices.length === 2 ? (
-      <OXButtons choices={currentQuestion.choices} setSelectedOption={setSelectedOption} selectedOption={selectedOption} />
+      <OXButtons
+        choices={currentQuestion.choices}
+        setSelectedOption={setSelectedOption}
+        selectedOption={selectedOption}
+      />
     ) : (
-      <OptionButtons choices={currentQuestion.choices} setSelectedOption={setSelectedOption} selectedOption={selectedOption} />
+      <OptionButtons
+        choices={currentQuestion.choices}
+        setSelectedOption={setSelectedOption}
+        selectedOption={selectedOption}
+      />
     );
   };
-
-
 
   return (
     <>
       <QuizContainer>
-
         <TextGroup>
           <h2 className="quiz__current">
             {currentIndex + 1}/{totalQuestions}
@@ -80,7 +83,9 @@ const Quiz = () => {
           <h3 className="quiz__question">{currentQuestion.questionText}</h3>
         </TextGroup>
 
-        <QuizButtonWrapper $direction={currentQuestion.choices.length === 2 ? "row" : "column"} >
+        <QuizButtonWrapper
+          $direction={currentQuestion.choices.length === 2 ? "row" : "column"}
+        >
           {renderChoices()}
         </QuizButtonWrapper>
 
@@ -91,15 +96,14 @@ const Quiz = () => {
         >
           정답확인
         </BaseButton>
-
       </QuizContainer>
 
-    {/* ✅ 개별 정답 모달 또는 최종 결과 모달 */}
-    {(isModalOpen || isLast) && (
-        <BaseModal 
-          isLast={isLast} 
-          score={score} 
-          isCorrect={isCorrect}  
+      {/* ✅ 개별 정답 모달 또는 최종 결과 모달 */}
+      {(isModalOpen || isLast) && (
+        <BaseModal
+          isLast={isLast}
+          score={score}
+          isCorrect={isCorrect}
           onNext={handleNext} // ✅ 최종 결과 모달이면 handleOut 실행
         />
       )}
