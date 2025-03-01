8257040d0877b84c2b319b5a073a041009ce6592
전세계 <kssjssk@gmail.com>
2025-03-02 00:19:40 +0900

    fefactor: 종목별 버튼 분리

---
+++ app/user/quiz/[id]/components/OXButtons.tsx
@@ -0,0 +1,41 @@
+import { OXSelectButton } from "@/app/user/quiz/[id]/components/Quiz.Styled";
+import { Circle, X } from "lucide-react";
+import { Choice } from "./mockData";
+
+interface OXButtonsProps {
+  choices: Choice[];
+  selectedOption: number;
+  setSelectedOption: (option: number) => void;
+}
+const OXButtons= ({ choices, selectedOption, setSelectedOption }: OXButtonsProps) => {
+  console.log("curent",choices);
+  
+  return (
+    
+    <>
+      {choices.map((choice) => (
+        <OXSelectButton 
+          key={choice.choiceId} type="button"
+          $isActive={selectedOption === choice.choiceNumber}
+          onClick={() => setSelectedOption(choice.choiceNumber)}
+        >
+          {choice.choiceText.toLowerCase() === "o" ? (
+            <Circle
+              color={ selectedOption === choice.choiceNumber ? "var(--white-color)" : "var(--primary-color)" }
+              strokeWidth={4}
+              size={60}
+            />
+          ) : (
+            <X 
+              color={ selectedOption === choice.choiceNumber? "var(--white-color)" : "var(--second-color)" }
+              strokeWidth={4}
+              size={68}
+            />
+          )}
+        </OXSelectButton>
+      ))}
+    </>
+
+  );
+};
+export default OXButtons;
