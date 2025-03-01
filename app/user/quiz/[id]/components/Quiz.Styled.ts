283325654e829b6f71663c6cdba4f16c9d2f4085
전세계 <kssjssk@gmail.com>
2025-03-02 00:18:47 +0900

    design: 퀴즈 메인 페이지

--- app/user/quiz/[id]/components/Quiz.Styled.ts
+++ app/user/quiz/[id]/components/Quiz.Styled.ts
@@ -1,52 +1,110 @@
 import styled from "styled-components";
 
 export const QuizContainer = styled.section`
-display: flex;
-flex-direction: column;
-    width: 100%;
-    height: 100%;
-    padding: 20px;
-`;
-
-export const ProgressWrapper = styled.div`
-    width: 100%;
-    height: 20px;
-    border-radius: 50px;
-    background: repeating-linear-gradient(
-      45deg, 
-      rgba(255, 255, 255, 0.2) 0px, 
-      rgba(255, 255, 255, 0.2) 10px, 
-      var(--primary-light) 10px, 
-      var(--primary-light) 20px
-    ), 
-    linear-gradient(to right, #4a90e2, #357abd);
-    background-size: 200% 100%;
-
-`;
-
-export const QuizText = styled.h6`
-    word-wrap: break-word;
-    white-space: pre-line;
-
-`;
-
-export const QuizButtonWrapper = styled.div`
   display: flex;
-  flex : 1;
-  justify-content: space-around;
+  flex-direction: column;
+  justify-content: space-between;
   align-items: center;
-  padding: 10px;
+
   width: 100%;
+  height: 100%;
+  min-height: 100%;
+
+  padding: 1.25rem;
+  gap: 1.25rem;
+
+  overflow-y: scroll;
 `;
 
-export const SelectButton = styled.button`
-    padding: 10px;
-    height: 10rem;
-    width: 10rem;
-    border: none;
-    background-color: var(--background-color);
-    border-radius: 10px;
+export const TextGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
+  width: 100%;
+
+  text-align: left;
+  word-wrap: break-word;
+  white-space: pre-wrap;
+
+  .quiz__current {
+    display: block;
+
+    width: 100%;
+
+    margin-bottom: 1rem;
+    padding-left: 0.625rem;
+
+    font-size: var(--font-size-2xl);
+    color: var(--primary-color);
+  }
+
+  .quiz__question {
+    display: block;
+
+    width: 100%;
+
+    margin-top: 1.875rem;
+    padding: 0 1.25rem 1.25rem 1.25rem;
+
+    font-size: var(--font-size-3xl);
+  }
 `;
 
-export const ResultButtonWrapper = styled.div``;
+export const QuizButtonWrapper = styled.div<{ $direction: "row" | "column" }>`
+  display: flex;
+  flex-direction: ${({ $direction }) => $direction};
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+
+  padding: 0.625rem;
+  gap: 1.875rem;
+`;
+
+export const OXSelectButton = styled.button<{ $isActive: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  
+  height: 10rem;
+  width: 10rem;
+
+  padding: 0.625rem;
+
+  background-color: ${({ $isActive }) => $isActive ? "var(--primary-200)" : "var(--background-color)"};
+  color: ${({ $isActive }) => ($isActive ? "var(--white-color)" : "")};
+
+  border: none;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -2px rgba(0, 0, 0, 0.1);
+`;
+
+export const OptionButton = styled.button<{ $isActive: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+
+  width: 100%;
+  min-height: 5rem;
+  height: auto;
+
+  padding: 1.25rem;
+
+  font-size: var(--font-size-xl);
+  font-weight: bold;
+
+  background-color: ${({ $isActive }) => $isActive ? "var(--primary-200)" : "" };
+  color: ${({ $isActive }) => ($isActive ? "white" : "")};
+
+  word-break: break-word;
+  white-space: pre-wrap;
+
+  border: none;
+  border-radius: 1.25rem;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -2px rgba(0, 0, 0, 0.1);
+`;
--- app/user/quiz/[id]/components/Quiz.tsx
+++ app/user/quiz/[id]/components/Quiz.tsx
@@ -1,32 +1,108 @@
 "use client";
-import * as S from "@/app/user/quiz/[id]/components/Quiz.Styled";
+import {QuizButtonWrapper, QuizContainer, TextGroup} from "@/app/user/quiz/[id]/components/Quiz.Styled";
 import BaseButton from "@/app/user/quiz/[id]/components/BaseButton";
 
+import ProgressBar from "@/app/user/quiz/[id]/components/ProgressBar";
+import OXButtons from "@/app/user/quiz/[id]/components/OXButtons";
+import OptionButtons from "@/app/user/quiz/[id]/components/OptionButtons";
+import { useState } from "react";
+import { mockQuizData, QuizData } from "@/app/user/quiz/[id]/components/mockData";
+import BaseModal from "@/app/user/quiz/[id]/components/BaseModal";
+import { useRouter } from "next/navigation";
+
+// 문제 5개를 가져와서 한페이지 안에서 출력
 const Quiz = () => {
+  // quiz mockData
+  const [quizData] = useState<QuizData[]>(mockQuizData);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [selectedOption, setSelectedOption] = useState<number>(0);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isCorrect, setIsCorrect] = useState<boolean>(false);
+  const [score, setScore] = useState<number>(0);
+  const [isLast, setIsLast] = useState<boolean>(false);
+  const router = useRouter();
+
+  const currentQuestion: QuizData = quizData[currentIndex];
+  const totalQuestions: number = quizData.length;
+  const isDisabled: boolean = selectedOption === 0;
+
+  // 정답 확인 함수
+  const handleResult = () => {
+    setIsModalOpen(true);
+    // 모달 오픈
+    // 정답 확인해서 모달에 전달
+    const correct = selectedOption === currentQuestion.answer;
+    setIsCorrect(correct);
+    if (correct) {
+        // 사용자가 푼 문제가 정답이면 server에 퀴즈 id를 전달
+        setScore((prev) => prev + 1);
+
+    }
+  };
+
+  const handleNext = () => {
+    setIsModalOpen(false);
+    if (currentIndex < totalQuestions - 1) {
+      setCurrentIndex((prev) => prev + 1);
+      setSelectedOption(0);
+    } else if (currentIndex === totalQuestions - 1){
+      setIsLast(true);
+    } else {
+      handleOut();
+    }
+  }
+
+  const handleOut = () => {
+    setIsModalOpen(false);
+    setIsLast(false);
+    router.push("/");
+  };
+
+  const renderChoices = () => {
+    return currentQuestion.choices.length === 2 ? (
+      <OXButtons choices={currentQuestion.choices} setSelectedOption={setSelectedOption} selectedOption={selectedOption} />
+    ) : (
+      <OptionButtons choices={currentQuestion.choices} setSelectedOption={setSelectedOption} selectedOption={selectedOption} />
+    );
+  };
+
+
+
   return (
     <>
-      <S.QuizContainer>
-        <S.ProgressWrapper>
+      <QuizContainer>
 
-        </S.ProgressWrapper>
-        <S.QuizText>
-          코리아 디스카운트는 한국 기업의 주식이 저평가되는 현상을 말한다.
-        </S.QuizText>
-        
-        <S.QuizButtonWrapper>
-          <S.SelectButton>
-          O
-          </S.SelectButton>
-          <S.SelectButton>
-          X
-          </S.SelectButton>
-        </S.QuizButtonWrapper>
+        <TextGroup>
+          <h2 className="quiz__current">
+            {currentIndex + 1}/{totalQuestions}
+          </h2>
+          <ProgressBar current={currentIndex + 1} />
+          <h3 className="quiz__question">{currentQuestion.questionText}</h3>
+        </TextGroup>
 
-        <S.ResultButtonWrapper>
-          <BaseButton $color="red">정답확인</BaseButton>
-        </S.ResultButtonWrapper>
+        <QuizButtonWrapper $direction={currentQuestion.choices.length === 2 ? "row" : "column"} >
+          {renderChoices()}
+        </QuizButtonWrapper>
 
-      </S.QuizContainer>
+        <BaseButton
+          $color="var(--primary-color)"
+          onClick={handleResult}
+          disabled={isDisabled}
+        >
+          정답확인
+        </BaseButton>
+
+      </QuizContainer>
+
+    {/* ✅ 개별 정답 모달 또는 최종 결과 모달 */}
+    {(isModalOpen || isLast) && (
+        <BaseModal 
+          isLast={isLast} 
+          score={score} 
+          isCorrect={isCorrect}  
+          onNext={handleNext} // ✅ 최종 결과 모달이면 handleOut 실행
+        />
+      )}
     </>
   );
 };
