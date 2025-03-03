09dbabee40f74d82539daa9cc1639fa2047b894b
전세계 <kssjssk@gmail.com>
2025-03-03 16:08:32 +0900

    remove: #47 분리를 잘못해서 삭제

--- application/usecases/quiz/interfaces/IGetChoicesUseCase.ts
+++
@@ -1,5 +0,0 @@
-import { ChoiceDto } from "@/application/usecases/quiz/dtos/ChoiceDto";
-
-export interface IGetChoicesUseCase {
-    execute(questionId: number): Promise<ChoiceDto[]>
-}
--- application/usecases/quiz/interfaces/IGetQuestionUseCase.ts
+++
@@ -1,5 +0,0 @@
-import { QuestionDto } from "@/application/usecases/quiz/dtos/QuestionDto";
-
-export interface IGetQuestionUseCase {
-    execute(questionId: number) :Promise<QuestionDto | null> ;
-}
