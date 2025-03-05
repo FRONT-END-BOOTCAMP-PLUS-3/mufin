e07a21bae90f194572de6f2b47015a3009cf934c
전세계 <kssjssk@gmail.com>
2025-03-05 23:04:10 +0900

    feat: 문제 덩답 시 usecase작성

--- application/usecases/quiz/SubmitQuizAnswerUseCase.ts
+++ application/usecases/quiz/SubmitQuizAnswerUseCase.ts
@@ -1,11 +1,17 @@
 import { ISubmitQuizAnswerUseCase } from "@/application/usecases/quiz/interfaces/ISubmitQuizAnswerUseCase";
 import { IRecordRepository } from "@/domain/repositories/IRecordRepository";
+import { IWalletRepository } from "@/domain/repositories/IWalletRepository";
 
 export class SubmitQuizAnswerUseCase implements ISubmitQuizAnswerUseCase {
 
-    constructor (private readonly recordRepository : IRecordRepository) {}
-    async execute(userId: string, questionId: number): Promise<void> {
+    constructor (
+        private readonly recordRepository : IRecordRepository,
+        private readonly walletRepository : IWalletRepository
+    ) {}
+    async execute(userId: string, questionId: number, reword:number): Promise<void> {
         await this.recordRepository.saveRecord(userId, questionId);
+
+        await this.walletRepository.updateAccountByUserId(userId, reword)
     }
 
 }
