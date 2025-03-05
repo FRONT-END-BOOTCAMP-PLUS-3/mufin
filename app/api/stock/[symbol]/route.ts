838694e5003281e818a6eb9e94b1291b9fa7e59f
전세계 <kssjssk@gmail.com>
2025-03-05 14:24:09 +0900

    rename: pr-> pg repository 이름 변경

--- app/api/stock/[symbol]/route.ts
+++ app/api/stock/[symbol]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { PrStockRepository } from '@/infrastructure/repositories/PrStockRepository';
+import { PrStockRepository } from '@/infrastructure/repositories/PgStockRepository';
 import { StockInfoUseCase } from '@/application/usecases/stock/StockInfoUseCase';
 
 
