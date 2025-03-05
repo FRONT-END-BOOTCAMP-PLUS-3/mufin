ed37ab751801b171cf35e9539266395e29b1ee1a
전세계 <kssjssk@gmail.com>
2025-03-05 14:24:45 +0900

    style: 장마감 시간 변경

--- utils/getMarketStatus.ts
+++ utils/getMarketStatus.ts
@@ -1,5 +1,5 @@
 const MARKET_OPEN_MINUTES = 9 * 60; // 9:00을 분으로 환산
-const MARKET_CLOSE_MINUTES = 15 * 60 + 30; // 15:30을 분으로 환산
+const MARKET_CLOSE_MINUTES = 15 * 60 + 15; // 15:30을 분으로 환산
 
 export function getMarketTimeStatus(): boolean {
   const now = new Date();
