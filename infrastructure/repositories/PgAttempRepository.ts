52e443084e5d70d7d916c644d898ba7a46586789
전세계 <kssjssk@gmail.com>
2025-03-05 14:25:43 +0900

    feature: attempt repository 작성

---
+++ infrastructure/repositories/PgAttempRepository.ts
@@ -0,0 +1,42 @@
+import { prisma } from "@/config/prismaClient";
+import { IAttemptRepository } from "@/domain/repositories/IAttempRespository";
+import { Attempt } from "@prisma/client";
+
+export class PgAttemptRepository implements IAttemptRepository{
+    /**
+     * @param userId 
+     * @returns 
+     */
+    async saveAttempt(userId: string): Promise<void> {
+        try {
+            await prisma.attempt.upsert({
+              where: { userId },
+              update: { quizDate: new Date() },
+              create: { userId, quizDate: new Date() },
+            });
+          } catch (error) {
+            console.error("Error in saveAttempt:", error);
+            throw error;
+          }
+    }
+    async findQuizDateByUserId(userId: string): Promise<Attempt | null> {
+        try{
+        return await prisma.attempt.findUnique({
+            where: { userId }
+        })
+    }catch (error){
+        console.error("Error in findQuizDateByUserId:", error);
+        throw error;
+    }
+    }
+    async deleteAttemptByUserId(userId: string): Promise<void> {
+        try{await  prisma.attempt.delete({
+            where: { userId}
+        })
+    } catch (error) {
+        console.error("Error in deleteAttemptByUserId:", error);
+        throw error;
+    }
+    }
+
+}
