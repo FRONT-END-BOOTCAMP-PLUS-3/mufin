dae0edee783f7c4c6677dceb54cc5fee75321775
전세계 <kssjssk@gmail.com>
2025-03-08 10:16:07 +0900

    refactor: #96 redisDB 중복 함수 제거 및 리팩토링

--- domain/repositories/IRedisRepository.ts
+++ domain/repositories/IRedisRepository.ts
@@ -1,8 +1,5 @@
 export interface IRedisRepository { 
-    setApprovalKey(apiKey: string, approvalKey: string): Promise<void> ;
-    getApprovalKey(apiKey: string): Promise<string | null>;
-    deleteApprovalKey(apiKey: string): Promise<void>;
-    setKISAccessToken(apiKey: string, kISAccessToken:string): Promise<void>;
-    getKISAccessToken(apiKey: string): Promise<string | null>
-    deleteKISAccessToken(apiKey: string): Promise<void>
+    saveKISValue(prefix: string, apiKey: string, value: string): Promise<void>
+    findKISValue(prefix: string, apiKey: string): Promise<string | null>
+    deleteKISValue(prefix: string, apiKey: string): Promise<void>
 }
