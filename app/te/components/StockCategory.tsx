6188d749e89b6a9942e251471baa878787b4b414
전세계 <kssjssk@gmail.com>
2025-03-10 00:33:31 +0900

    rename: 107파일 경로명 변경

--- app/te/components/StockCategory.tsx
+++ app/te/components/StockCategory.tsx
@@ -8,15 +8,15 @@ import {
     CategoryCard,
     CategoryName,
     CategoryWrapper,
-} from "@/app/components/home/StockCategory.Styled";
+} from "@/app/te/components/StockCategory.Styled";
 
 const categories = [
-    { id: 1,   name: "스마트폰", image: "/stock-category/smartphone.svg", path: "/stocks/smartphone", color: "#FFB5FA"},
-    { id: 2,  name: "자동차", image: "/stock-category/automobile.svg", path: "/stocks/automobile", color: "#B1B8FF" },
-    { id: 3,  name: "식품", image: "/stock-category/food.svg", path: "/stocks/food", color: "#FFD6A8" },
-    { id: 4,  name: "엔터테인먼트", image: "/stock-category/entertainment.svg",path: "/stocks/entertainment",color: "#A5EAFF"},
-    { id: 5,  name: "제약", image: "/stock-category/pharmaceutical.svg", path: "/stocks/pharmaceutical", color: "#B4FFBC" },
-    { id: 6,  name: "반도체", image: "/stock-category/semiconductor.svg", path: "/stocks/semiconductor", color: "#FF8D8D" },
+    { id: 1,   name: "스마트폰", image: "/stock-category/smartphone.svg",  color: "#FFB5FA"},
+    { id: 2,  name: "자동차", image: "/stock-category/automobile.svg", color: "#B1B8FF" },
+    { id: 3,  name: "식품", image: "/stock-category/food.svg",  color: "#FFD6A8" },
+    { id: 4,  name: "엔터테인먼트", image: "/stock-category/entertainment.svg",color: "#A5EAFF"},
+    { id: 5,  name: "제약", image: "/stock-category/pharmaceutical.svg",  color: "#B4FFBC" },
+    { id: 6,  name: "반도체", image: "/stock-category/semiconductor.svg",  color: "#FF8D8D" },
 ]
 
 const StockCategory = () => {
@@ -30,7 +30,6 @@ const StockCategory = () => {
                             <CategoryCard 
                             color={category.color} 
                             href={{ pathname: "/category", query: { c: category.id } }}
-                            passHref
                         >
                                     <Image src={category.image} alt={category.name} width={80} height={20} />
                             </CategoryCard>
