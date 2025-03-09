ca4d59e64a2142df31105ea9143ea263d0541b75
전세계 <kssjssk@gmail.com>
2025-03-09 23:55:09 +0900

    feature: #107 category 별 stockList usecase및 route.ts작성

---
+++ application/usecases/stock/StockByCategoryUseCase.ts
@@ -0,0 +1,43 @@
+import { IStockByCategoryUseCase } from "@/application/usecases/stock/interfaces/IStcokByCategoryUseCase";
+import { IStockRepository } from "@/domain/repositories/IStockRepository";
+import { IGetCurrentPriceUseCase } from "@/application/usecases/kis/interfaces/IGetCurrentPriceUseCase";
+import { Stock } from "@prisma/client";
+import { StockListResponseDto } from "@/application/usecases/home/dtos/StockListResponseDto";
+
+export class StockByCategoryUseCase implements IStockByCategoryUseCase{
+    
+    constructor(
+        private readonly stockRepository: IStockRepository,
+        private readonly getCurrentPriceUseCase: IGetCurrentPriceUseCase
+    ) {}
+
+    async execute(category: string) : Promise<StockListResponseDto[]> {
+        const categoryNum = parseInt(category, 10);
+        const stocks: Stock[] = await this.stockRepository.findStocksByCategory(categoryNum);
+
+        if(stocks.length === 0) {
+            throw new Error("not found StocksByCategory in useCases");
+        }
+        const updatedStocks: StockListResponseDto[] = await Promise.all(
+        stocks.map(async(stock, index)=>{
+            const currentPrice = await this.getCurrentPriceUseCase.execute(stock.stockCode);
+
+            if(!currentPrice ){
+                throw new Error("not found currentPrice in kis usecases");
+            }
+
+            return {
+                index:index+1,
+                stockId: stock.stockId,
+                stockCode: stock.stockCode,
+                stockName: stock.stockName,
+                category: stock.category || 0,
+                stockImage: stock.stockImage || "",
+                currentPrice, 
+            }
+        }));
+
+        return updatedStocks;
+    }
+
+}
