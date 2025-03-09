3fa9e5c24d025c6226990ad3d279d8e8dd3cdc58
전세계 <kssjssk@gmail.com>
2025-03-09 15:38:59 +0900

    refactor: 분봉 데이터 비즈니스로직 usecase로 변경

--- app/api/stock/min_chart/route.ts
+++ app/api/stock/min_chart/route.ts
@@ -1,57 +1,25 @@
-import { AccessTokenUseCase } from "@/application/usecases/kis/AccessTokenUseCase";
-import { IAccessTokenUseCase } from "@/application/usecases/kis/interfaces/IAccessTokenUseCase";
-import { fetchKISMinChart } from "@/infrastructure/api/kisApiClient";
 import { NextResponse } from "next/server";
+import { StockChartDto } from "@/application/usecases/kis/dtos/StockChartDto";
+import { GetMinChartUseCase } from "@/application/usecases/kis/GetMinChartUseCase";
+import { IGetMinChartUseCase } from "@/application/usecases/kis/interfaces/IGetMinChartUseCase";
+
 
 export async function GET(req: Request) {
   try {
-    const kisAccessTokenUseCase :IAccessTokenUseCase = new AccessTokenUseCase(); 
-    const KISAccessToken = await kisAccessTokenUseCase.execute();
+    const symbol = req.url.split('?')[1].split('&')[0].split('=')[1];
+    
+    const getMinChartUseCase: IGetMinChartUseCase = new GetMinChartUseCase();
 
-    if (!KISAccessToken) {
-      return NextResponse.json({ error: "KISAccessToken not found in cookies" }, { status: 400 });
+    const stockChartDtos: StockChartDto[] = await getMinChartUseCase.execute(symbol);
+
+    if(stockChartDtos.length === 0 ) {
+      return NextResponse.json({ message: 'KIS API에서 데이터를 가져오는데 실패하였습니다.' }, { status: 500 });  // KIS API에서 에러가 발생하였을 경우 500 Internal Server Error 반환  // TODO: 에러 로깅 or KIS API 에러 처리 로직 추가 필요  // TODO: KIS API 에러 로깅 or KIS API 에러 처리
     }
 
-
-    const getCurrentTime = (offset: number): string => {
-      const now = new Date();  
-      const marketCloseTime = new Date();
-      marketCloseTime.setHours(15, 20, 0, 0);
-      const marketOpenTime = new Date();
-      marketOpenTime.setHours(9, 0, 0, 0); 
-
-      let resultTime = now;
-      if (now >= marketCloseTime || now < marketOpenTime) {
-        resultTime = new Date();
-        resultTime.setHours(15, 20, 0, 0);
-        resultTime.setMinutes(resultTime.getMinutes() - offset * 31); 
-      } else {
-        resultTime.setMinutes(resultTime.getMinutes() - offset * 31);
-      }
-
-      const hours = resultTime.getHours().toString().padStart(2, '0');
-      const minutes = resultTime.getMinutes().toString().padStart(2, '0');
-
-      return `${hours}${minutes}00`;  
-    };
-    
-    const symbol = req.url.split('?')[1].split('&')[0].split('=')[1];
-
-    const requests = Array.from({ length: 4 }, async (_, i) => {
-      const formattedTime = getCurrentTime(i);
-
-
-      console.log('Processing time:', formattedTime);
-
-      return fetchKISMinChart(symbol, formattedTime);
-
-    });
-
-    const data = await Promise.all(requests);  
-    return new Response(JSON.stringify(data), { status: 200 });
+    return NextResponse.json(stockChartDtos, { status: 200 });
   } catch (error: unknown) {
     console.error('API 요청 실패:', error);
     const errorMessage = error instanceof Error ? error.message : 'Unknown error';
-    return new Response(JSON.stringify({ message: '데이터 가져오기 실패', error: errorMessage }), { status: 500 });
+    return NextResponse.json({ message: '데이터 가져오기 실패', error: errorMessage }, { status: 500 });
   }
 }
