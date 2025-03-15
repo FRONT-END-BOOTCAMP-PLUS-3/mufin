5a735f11c50beb6235b5a4486ebea8f45e23ed45
전세계 <kssjssk@gmail.com>
2025-03-15 15:57:34 +0900

    style: approvalKey분리를 위한 독립적인 함수로변경

--- domain/repositories/IRedisRepository.ts
+++ domain/repositories/IRedisRepository.ts
@@ -1,5 +1,10 @@
+import { ApprovalKeyEntity } from "@/domain/entities/ApprovalKeyEntity";
 export interface IRedisRepository { 
-    saveKISValue(prefix: string, apiKey: string, value: string): Promise<void>
-    findKISValue(prefix: string, apiKey: string): Promise<string | null>
-    deleteKISValue(prefix: string, apiKey: string): Promise<void>
+    saveKISApprovalKeyData(prefix: string, apiKey: string, value: ApprovalKeyEntity): Promise<void>;
+    findKISApprovalKeyData(prefix: string, apiKey: string): Promise<ApprovalKeyEntity | null>;
+    deleteKISApprovalKeyData(prefix: string, apiKey: string): Promise<void>;
+    updateInUseWithoutResetTTL(prefix: string, apiKey: string,  newInUse: boolean): Promise<string | null>;
+    saveKISAccessToken(prefix: string, apiKey: string, value: string): Promise<void>;
+    findKISAccessToken(prefix: string, apiKey: string): Promise<string | null>;
+    deleteKISAccessToken(prefix: string, apiKey: string): Promise<void>;
 }
