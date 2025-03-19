bd8a9bdcc58883cf4d53de10514b9187e9e277d9
전세계 <kssjssk@gmail.com>
2025-03-19 14:24:25 +0900

    refactor : #1 quizDI container 리팩토링

--- app/api/user/quiz/route.ts
+++ app/api/user/quiz/route.ts
@@ -1,18 +1,9 @@
-import { ResponseQuizDto } from "@/application/usecases/quiz/dtos/ResponseQuizDto";
-import { GetRandomQuestionUseCase } from "@/application/usecases/quiz/GetQuestionUseCase";
-import { IGetRandomQuestionUseCase } from "@/application/usecases/quiz/interfaces/IGetRandomQuestionUseCase";
-import { ISubmitQuizAnswerUseCase } from "@/application/usecases/quiz/interfaces/ISubmitQuizAnswerUseCase";
-import { SubmitQuizAnswerUseCase } from "@/application/usecases/quiz/SubmitQuizAnswerUseCase";
-import { IChoiceRepository } from "@/domain/repositories/IChoiceRepository";
-import { IQuestionRepository } from "@/domain/repositories/IQuestionRepository";
-import { IRecordRepository } from "@/domain/repositories/IRecordRepository";
-import { IWalletRepository } from "@/domain/repositories/IWalletRepository";
-import { PgChoiceRepository } from "@/infrastructure/repositories/PgChoiceRepository";
-import { PgQuestionRepository } from "@/infrastructure/repositories/PgQuestionRepositroy";
-import { PgRecordRepository } from "@/infrastructure/repositories/PgRecordRepository";
-import { PgWalletRepository } from "@/infrastructure/repositories/PgWalletRepository";
 import { NextRequest, NextResponse } from "next/server";
+import { quizDi } from "@/infrastructure/config/quizDi";
+import { ResponseQuizDto } from "@/application/usecases/quiz/dtos/ResponseQuizDto";
 import { getDecodedUserId } from "@/utils/getDecodedUserId";
+
+
 
 // HTTP Method GET : 퀴즈 문제 받아오기
 export async function GET(req: NextRequest) {
@@ -30,19 +21,9 @@ export async function GET(req: NextRequest) {
     if (!userId) {
       return NextResponse.json({ error: "User ID not found in cookies" }, { status: 400 });
     }
-    
-
-    // repository 인스턴스 생성
-    const questionRepository: IQuestionRepository = new PgQuestionRepository();
-    const recordRepository: IRecordRepository = new PgRecordRepository();
-    const choiceRepository: IChoiceRepository = new PgChoiceRepository();
-
-
-    const getRandomQuestionUseCase: IGetRandomQuestionUseCase =
-      new GetRandomQuestionUseCase(questionRepository, recordRepository, choiceRepository);
 
     const responseQuizDtos: {totalQuestions: number; questions:ResponseQuizDto[]} 
-      = await getRandomQuestionUseCase.execute(userId, limit);
+      = await quizDi.getRandomQuestionUseCase.execute(userId, limit);
 
     return NextResponse.json( {quiz:responseQuizDtos} , { status: 200 });
 
@@ -77,17 +58,8 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: "Missing reword" }, { status: 400 });
     }
 
-    // Repository 인스턴스 생성
-    const recordRepository: IRecordRepository = new PgRecordRepository();
-    const walletRepository: IWalletRepository = new PgWalletRepository();
-
-    // Repository 의존성주입
-    const submitQuizAnswerUseCase: ISubmitQuizAnswerUseCase = new SubmitQuizAnswerUseCase(recordRepository, walletRepository);
-
     // Submit question Answer UseCase 동작
-    await submitQuizAnswerUseCase.execute(userId, questionId, reword);
-
-
+    await quizDi.submitQuizAnswerUseCase.execute(userId, questionId, reword);
 
     return NextResponse.json({status:200});
     // 에러 시 동작 에러코드 반환
