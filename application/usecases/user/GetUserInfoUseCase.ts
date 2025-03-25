9b2ef41cb05518185d35c7e7bab947264037ab93
전세계 <kssjssk@gmail.com>
2025-03-25 21:23:20 +0900

    refactor/#3 refresh token 재발급 로직 분리로 인한 중복 코드 제거

--- application/usecases/user/GetUserInfoUseCase.ts
+++ application/usecases/user/GetUserInfoUseCase.ts
@@ -1,76 +1,20 @@
-import jwt from "jsonwebtoken";
 import { IUserRepository } from "@/domain/repositories/IUserRepository";
-import { RefreshAccessTokenUseCase } from "./RefreshAccessTokenUseCase";
-import { JwtPayload } from "jsonwebtoken";
-import { env } from "@/config/env";
-
-export interface CustomJwtPayload extends JwtPayload {
-  userId: string;
-  loginId: string;
-}
-
-export interface GetUserInfoResult {
-  user: { loginId: string; name: string };
-  newTokenCookie?: string;
-}
+import { User } from "@prisma/client";
 
 export class GetUserInfoUseCase {
   constructor(private userRepository: IUserRepository) {}
 
-  async execute(
-    accessToken: string | undefined,
-    refreshToken: string | undefined
-  ): Promise<GetUserInfoResult> {
-    if (!accessToken && !refreshToken) {
+  async execute(userId: string): Promise<User> {
+    if (!userId) {
       throw new Error("Unauthorized");
     }
 
-    let decoded: CustomJwtPayload | undefined = undefined;
-    let newTokenCookie: string | undefined = undefined;
-
-    // 1. Access Token 검증 시도
-    if (accessToken) {
-      try {
-        decoded = jwt.verify(
-          accessToken, env.JWT_SECRET as string
-        ) as CustomJwtPayload;
-      } catch (error) {
-        // 액세스 토큰이 만료되거나 유효하지 않으면, refresh token 처리로 넘어감.
-        console.error(error);
-      }
-    }
-
-    // 2. Access Token이 없거나 유효하지 않으면 refreshToken으로 새 토큰 발급 시도
-    if (!decoded && refreshToken) {
-      const refreshAccessTokenUseCase = new RefreshAccessTokenUseCase(
-        this.userRepository
-      );
-      const refreshResult = await refreshAccessTokenUseCase.execute(
-        refreshToken
-      );
-      accessToken = refreshResult.accessToken;
-      newTokenCookie = refreshResult.newTokenCookie;
-      decoded = jwt.verify(
-        accessToken,
-        env.JWT_SECRET as string
-      ) as CustomJwtPayload;
-    }
-
-    if (!decoded) {
-      throw new Error("Invalid accessToken");
-    }
-
-    const loginId: string = decoded.loginId;
-
     // 3. DB에서 사용자 정보 조회
-    const user = await this.userRepository.findByLoginId(loginId);
+    const user = await this.userRepository.findUserByUserId(userId);
+    
     if (!user) {
       throw new Error("User not found");
     }
-
-    return {
-      user: { loginId: user.loginId, name: user.name },
-      newTokenCookie,
-    };
+    return user;
   }
 }
