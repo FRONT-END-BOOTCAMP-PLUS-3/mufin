3f5961ffc1e43a3c9d82bd605ebbb61c18504b58
전세계 <kssjssk@gmail.com>
2025-04-03 23:04:27 +0900

    comment: 사용하지 않는 주석 제거

--- app/(anon)/category/components/Category.tsx
+++ app/(anon)/category/components/Category.tsx
@@ -1,7 +1,6 @@
 
 import { CategoryContainer } from "@/app/(anon)/category/components/Category.Styled";
 import StockList from "@/app/components/home/StockList";
-// import { CategoryPageProps } from "@/app/(anon)/category/page";
 
 interface CategoryProps {
   categoryId: string; // 부모에서 이미 문자열로 확정해서 넘겨줌
