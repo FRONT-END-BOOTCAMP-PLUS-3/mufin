0dc55ba2d89a2b4ae640b80ca4b62745c095d96e
전세계 <kssjssk@gmail.com>
2025-04-03 23:02:01 +0900

    setting: react query Provider 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,49 +3,68 @@ import StyledComponentsRegistry from "@/lib/StyledComponentsRegistry";
 import LayoutClient from "@/app/components/LayoutClient";
 import { Suspense } from "react";
 import LoadingPage from "@/app/components/home/LoadingPage";
+import ReactQueryProvider from "@/providers/ReactQueryProvider";
+import { DehydratedState, HydrationBoundary } from '@tanstack/react-query';
 
-if (process.env.NODE_ENV === 'production') {
-  console.log = () => {};  // console.log() 숨기기
-  console.error = () => {};  // console.error() 숨기기
-  console.warn = () => {};   // console.warn() 숨기기
+
+if (process.env.NODE_ENV === "production") {
+  console.log = () => {}; // console.log() 숨기기
+  console.error = () => {}; // console.error() 숨기기
+  console.warn = () => {}; // console.warn() 숨기기
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+interface RootLayoutProps {
   children: React.ReactNode;
-}>) {
+  dehydratedState: DehydratedState;
+}
+
+export default function RootLayout({ children, dehydratedState }:RootLayoutProps) {
   return (
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="계좌 개설 없이 쉽게 배울 수 있는 주식 공부 어플로, 누구나 쉽게 주식 시장을 이해하고 학습할 수 있습니다." />
-        <meta name="keywords" content="주식 공부, 주식, 투자, 주식 시장, 초보자, 주식 교육" />
+        <meta
+          name="description"
+          content="계좌 개설 없이 쉽게 배울 수 있는 주식 공부 어플로, 누구나 쉽게 주식 시장을 이해하고 학습할 수 있습니다."
+        />
+        <meta
+          name="keywords"
+          content="주식 공부, 주식, 투자, 주식 시장, 초보자, 주식 교육"
+        />
         <meta name="robots" content="index, follow" />
-        
-        <meta property="og:type" content="website"/>
+
+        <meta property="og:type" content="website" />
 
         <meta property="og:type" content="website" />
         <meta property="og:title" content="주식 투자 플랫폼" />
-        <meta property="og:description" content="계좌 개설 없이 쉽게 배울 수 있는 주식 공부 어플로, 누구나 쉽게 주식 시장을 이해하고 학습할 수 있습니다." />
+        <meta
+          property="og:description"
+          content="계좌 개설 없이 쉽게 배울 수 있는 주식 공부 어플로, 누구나 쉽게 주식 시장을 이해하고 학습할 수 있습니다."
+        />
         <meta property="og:url" content="https://mufin.newlecture.com" />
-        
+
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="주식 투자 플랫폼" />
-        <meta name="twitter:description" content="계좌 개설 없이 쉽게 배울 수 있는 주식 공부 어플로, 누구나 쉽게 주식 시장을 이해하고 학습할 수 있습니다." />
+        <meta
+          name="twitter:description"
+          content="계좌 개설 없이 쉽게 배울 수 있는 주식 공부 어플로, 누구나 쉽게 주식 시장을 이해하고 학습할 수 있습니다."
+        />
 
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="canonical" href="https://mufin.newlecture.com"></link>
         <title>주식 투자 플랫폼</title>
-        
       </head>
       <body>
-        <StyledComponentsRegistry>
-          <Suspense fallback={<LoadingPage />}>
-            <LayoutClient> {children} </LayoutClient>
-          </Suspense>
-        </StyledComponentsRegistry>
+        <ReactQueryProvider>
+        <HydrationBoundary state={dehydratedState}>
+          <StyledComponentsRegistry>
+            <Suspense fallback={<LoadingPage />}>
+              <LayoutClient> {children} </LayoutClient>
+            </Suspense>
+          </StyledComponentsRegistry>
+          </HydrationBoundary>
+        </ReactQueryProvider>
       </body>
     </html>
   );
---
+++ providers/ReactQueryProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+    defaultOptions: { 
+        queries: {
+            staleTime: 1000 * 30 , // 30초
+            refetchOnWindowFocus: false, // 윈도우 포커스 시 자동 재요청
+            refetchInterval: 1000 * 60, // 60초마다 데이터 자동 재요청
+            retry: 1,
+        },
+    },
+});
+
+interface ProviderProps {
+    children: React.ReactNode;
+}
+
+const ReactQueryProvider = ({ children }: ProviderProps) => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={true} />
+        </QueryClientProvider>
+    );
+}
+export default ReactQueryProvider;
