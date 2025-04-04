b331c3b47ad919a48b4bca363dbe9189b68c27c6
전세계 <kssjssk@gmail.com>
2025-04-04 11:07:37 +0900

    hotfix/새로고침 시 react-query데이터 중복 에러 해결

--- providers/ReactQueryProvider.tsx
+++ providers/ReactQueryProvider.tsx
@@ -8,6 +8,7 @@ const queryClient = new QueryClient({
     defaultOptions: { 
         queries: {
             staleTime: 1000 * 30 , // 30초
+            refetchOnMount: false,
             refetchOnWindowFocus: false, // 윈도우 포커스 시 자동 재요청
             refetchInterval: 1000 * 60, // 60초마다 데이터 자동 재요청
             retry: 1,
